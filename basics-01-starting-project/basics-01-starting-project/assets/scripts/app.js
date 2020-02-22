4dae568a03002a60e48e903bc032b644205e6227
Jheide22 <jheide22@gmail.com>
2020-02-21 22:48:21 -0500

    newFix

--- basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -4,5 +4,5 @@ let currentResult = defaultResult;
 currentResult = currentResult + 10;
 
 let calculationDesc = '' + currentResult + ' + 10'; 
-
+let errorMsg = 'ERROR';
 outputResult(currentResult, calculationDesc); 
