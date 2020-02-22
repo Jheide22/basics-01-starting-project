2ba484a882ef6b67a51a5f8a22e1bbd118ba869e
Jheide22 <jheide22@gmail.com>
2020-02-21 22:09:11 -0500

    1

--- basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -1,6 +1,6 @@
 alert("It works!");
-let currentResult = 0;
-
+let currentResult = 10;
+let calculationDesc = '' +currentResult + ' + 10'
 currentResult = currentResult + 10;
 
-outputResult(currentResult, '');
+outputResult(currentResult, calculationDesc);
