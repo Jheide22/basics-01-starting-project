df0124600ffa2b7d2fe44fa8bdb19fc4153a4ea8
Jonathan Heide <jheide22@gmail.com>
2020-02-23 19:07:42 -0500

    added divide

--- basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -1,19 +1,23 @@
 const defaultResult = 0;
-let currentResult;
+let currentResult = defaultResult;
 
 function add(){
     currentResult = currentResult + userInput.value;
-    outputResult(currentResult, calculationDesc); 
+    outputResult(currentResult, ''); 
 }
 function subtract(){
     currentResult = currentResult - userInput.value;
-    outputResult(currentResult, calculationDesc); 
+    outputResult(currentResult, ''); 
 }
 function multiply(){
     currentResult = currentResult * userInput.value;
-    outputResult(currentResult, calculationDesc); 
+    outputResult(currentResult, ''); 
 }
-
+function divide(){
+    currentResult = currentResult * userInput.value;
+    outputResult(currentResult, ''); 
+}
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
