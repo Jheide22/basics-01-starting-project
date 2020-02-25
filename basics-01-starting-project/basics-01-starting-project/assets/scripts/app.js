4fe1a6868238e879e1423e7c968989c1dabad5fd
Jonathan Heide <jheide22@gmail.com>
2020-02-25 13:09:06 -0500

    addeding logging objs

--- basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -5,6 +5,20 @@ let currentResult = defaultResult;
 function getUserInput() {
     return parseInt(userInput.value);
 }
+function writeToLog(
+    operationidentifier,
+    prevResult,
+    operationNumber,
+    newResult){
+        const logEntry = {
+            operation: operationidentifier,
+            prevResult: prevResult,
+            number: operationNumber,
+            result: newResult
+        };
+        logEntries.push(logEntry);
+        console.log(logEntries);
+}
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription);
@@ -14,26 +28,28 @@ function add(){
     const initialResult = currentResult;
     currentResult += eneteredNumber; 
     createAndWriteOutput('+', initialResult, eneteredNumber);
-    logEntries.push(eneteredNumber);
-    console.log(logEntries);
+    writeToLog('ADD', initialResult, eneteredNumber, currentResult);
 }
 function subtract(){
     const eneteredNumber = getUserInput();
     const initialResult = currentResult;
     currentResult -= eneteredNumber;
     createAndWriteOutput('-', initialResult, eneteredNumber);
+    writeToLog('SUBTRACT', initialResult, eneteredNumber, currentResult);
 }
 function multiply(){
     const eneteredNumber = getUserInput();
     const initialResult = currentResult;
     currentResult *= eneteredNumber; 
     createAndWriteOutput('*', initialResult, eneteredNumber);
+    writeToLog('MULTIPLY', initialResult, eneteredNumber, currentResult);
 }
 function divide(){
     const eneteredNumber = getUserInput();
     const initialResult = currentResult;
     currentResult /= eneteredNumber; 
     createAndWriteOutput('/', initialResult, eneteredNumber);
+    writeToLog('DIVIDE', initialResult, eneteredNumber, currentResult);
 }
 
 addBtn.addEventListener('click', add);
